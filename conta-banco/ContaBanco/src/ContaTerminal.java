72dd081992d62d6afee981496fcabaad35c24ed2
Pedro Fava <pedrofava2004@gmail.com>
2023-09-10 03:28:15 -0300

    add: first java project

---
+++ conta-banco/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class ContaTerminal{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite o numero da conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Digite o numero da agencia: ");
+        String agencia = scanner.next();
+
+        scanner.nextLine();
+
+        System.out.println("Digite o nome do titular: ");
+        String titular = scanner.nextLine();
+
+        System.out.println("Digite o saldo da conta: ");
+        double saldo = scanner.nextDouble();
+
+System.out.println("Ola, " + titular + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia +" , conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+    }
+} 
