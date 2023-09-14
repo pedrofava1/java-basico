53c9aa7c4d256641094b343a9152fa7a89f5513f
pedrofava1 <pedro.fava@unifesp.br>
2023-09-13 21:44:12 -0300

    add: try catch

---
+++ terminal/MinhaClasse/src/FormatadorCep.java
@@ -0,0 +1,17 @@
+public class FormatadorCep {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("2365064");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("Ocorreu um erro ao formatar o cep");
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+          throw new CepInvalidoException();
+        
+          //simulando um cep formatado
+          return "23.765-064";
+    }
+}
